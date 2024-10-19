07b461e3379aee14cf4411abcace6f3d67172ca6
Minhoangg <132485081+Minhoangg@users.noreply.github.com>
2024-10-19 13:11:32 +0700

    update

--- routes/admin/brand/brand.php
+++ routes/admin/brand/brand.php
@@ -2,13 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 
-use App\Http\Controllers\admin\brand\brandController;
+use App\Http\Controllers\admin\brand\BrandController;
 
 Route::prefix('/brand')->group(function () {
-    Route::get('/list', [brandController::class, 'index']); //->middleware('auth.jwt')
-    Route::get('/update/{id}', [brandController::class, 'update']); //->middleware('auth.jwt')
-    Route::put('/update/{id}', [brandController::class, 'updateData']);
-    Route::delete('/delete/{id}', [brandController::class, 'destroy']); //->middleware('auth.jwt')
-    Route::get('/data-for-create', [brandController::class, 'getCategories']); //->middleware('auth.jwt')
-    Route::post('/create', [brandController::class, 'store']); //->middleware('auth.jwt')
+    Route::get('/list', [BrandController::class, 'index']); //->middleware('auth.jwt')
+    Route::get('/update/{id}', [BrandController::class, 'update']); //->middleware('auth.jwt')
+    Route::put('/update/{id}', [BrandController::class, 'updateData']);
+    Route::delete('/delete/{id}', [BrandController::class, 'destroy']); //->middleware('auth.jwt')
+    Route::get('/data-for-create', [BrandController::class, 'getCategories']); //->middleware('auth.jwt')
+    Route::post('/create', [BrandController::class, 'store']); //->middleware('auth.jwt')
 });
